85b80a420ed512bbb5426366d6c9bf7532ec669a
amine <amine.mezghich@ensi-uma.tn>
2021-01-22 15:54:55 +0100

    crud wihtout service

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,18 +8,58 @@ import { HttpClient } from '@angular/common/http';
 export class PostsComponent implements OnInit {
 
   posts;
-  constructor(Http: HttpClient) {
+  private url = 'http://jsonplaceholder.typicode.com/posts';
 
-    Http.get('http://jsonplaceholder.typicode.com/posts').subscribe(
+  constructor(private Http: HttpClient) {
+
+    
+
+   }
+
+  ngOnInit(): void {
+    this.Http.get(this.url).subscribe(
       response => {
         this.posts = response;
         console.log(this.posts)
       }
     )
-
-   }
-
-  ngOnInit(): void {
   }
+
+  createPost(title) { 
+    let post : any = {'title':title.value};
+    this.Http.post(this.url, post).subscribe( 
+      response => {
+        post['id'] = response['id'];
+        post['userId'] = response['userId'];
+        this.posts.splice(0, 0, post);
+        // we can see if we update posts, thanks to the method Subscribe, we get notified of this modifiations
+        //console.log(this.posts.splice(0,0,post));
+      }
+    )
+  }
+
+  updatePost(post) { 
+    post['title'] = "java";
+    post['body'] = "Très bon langage";
+    this.Http.put(this.url+'/'+post['id'], post).subscribe(
+      response => {
+        //post = response;
+        console.log(response);
+      }
+    )
+
+  }
+
+  deletePost(post) {
+    this.Http.delete(this.url + '/' + post['id']).subscribe(
+      response => {
+        let index = this.posts.indexOf(post);
+        this.posts.splice(index,1)
+        console.log(response);
+      }
+    )
+  }
+
+
 
 }
